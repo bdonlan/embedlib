83d4d9c5f8c40a6fd88f0d4c7af6d141c6aa8bdd
Bryan Donlan <bdonlan@fushizen.net>
2008-12-11 20:50:09 -0500

    Include stdlib.h for size_t

--- stub.h
+++ stub.h
@@ -1,6 +1,8 @@
 #ifndef H_##HEADERNAME##
 #define H_##HEADERNAME##
 
+#include <stdlib.h>
+
 const void *##PREFIX##lookup(const char *name, size_t *out_filesize);
 void ##PREFIX##forall(
 		void (*iter)(const char *name, const void *data, size_t size)
